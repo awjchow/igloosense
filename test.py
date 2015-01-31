c10aa6b76e61ef582b0b97769c10de5f6389e9cb
awjchow <Anthony>
2015-01-31 15:06:11 -0500

    add

--- test.py
+++ test.py
@@ -1,29 +1,65 @@
 import sys
 
 import Adafruit_DHT
+import RPi.GPIO as GPIO
+import time
+
+def Resolve_Light(score):
+    LOWEST = 70     #brightest
+    HIGHEST = 175   #darkest
+    score = (score - LOWEST) / float(HIGHEST - LOWEST)
+    return score
+
+def RC_Analog(pin):
+	counter = 0
+	
+	#Discharge capicator, set output to low
+	GPIO.setup(pin,GPIO.OUT)
+	GPIO.output(pin,GPIO.LOW)
+
+	time.sleep(1)
+	print "Complete discharging of capacitance\n"
+	GPIO.setup(pin,GPIO.IN)
+	#count loops until voltage across capicator reads high on GPIO
+	while(GPIO.input(pin)==GPIO.LOW):
+		#print "My Pin current: ", GPIO.input(pin)
+		counter += 1
+		#time.sleep(0.1)
+	print GPIO.input(pin)
+	return Resolve_Light(counter)
 
 
-# Parse command line parameters.
-sensor_args = { '11': Adafruit_DHT.DHT11,
-    '22': Adafruit_DHT.DHT22,
-    '2302': Adafruit_DHT.AM2302 }
-if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-	sensor = sensor_args[sys.argv[1]]
-	pin = sys.argv[2]
-else:
-	print 'usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#'
-	print 'example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4'
-	sys.exit(1)
+def main():
+	GPIO.setmode(GPIO.BCM)
+	# Parse command line parameters.
+	sensor_args = { '11': Adafruit_DHT.DHT11,
+	    '22': Adafruit_DHT.DHT22,
+	    '2302': Adafruit_DHT.AM2302 }
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
-if humidity is not None and temperature is not None:
-	print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
-else:
-	print 'Failed to get reading. Try again!'
+	# Try to grab a sensor reading.  Use the read_retry method which will retry up
+	# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+	TEMP_SENSOR = '22'
+	TEMP_SENSING_PIN = '17'
+	
+
+	humidity, temperature = Adafruit_DHT.read_retry(TEMP_SENSOR, TEMP_SENSING_PIN)
+
+	# Note that sometimes you won't get a reading and
+	# the results will be null (because Linux can't
+	# guarantee the timing of calls to read the sensor).
+	# If this happens try again!
+	if humidity is not None and temperature is not None:
+		print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
+	else:
+		print 'Failed to get reading. Try again!'
+
+	LIGHT_SENSING_PIN = 26
+	print "My measurement of strength of light via counting of loops :",RC_Analog(LIGHT_SENSING_PIN)
+
+
+
+
+
+if __name__ == '__main__':
+	main()
