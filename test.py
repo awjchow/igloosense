889b4b144efc3b09aa1e7aa9ee7a2007a989b4fe
awjchow <Anthony>
2015-01-31 15:24:57 -0500

    add

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import sys
 import Adafruit_DHT
 import RPi.GPIO as GPIO
 import time
+import datetime
+
 
 def Resolve_Light(score):
     LOWEST = 70     #brightest
@@ -15,17 +17,29 @@ def RC_Analog(pin):
 	#Discharge capicator, set output to low
 	GPIO.setup(pin,GPIO.OUT)
 	GPIO.output(pin,GPIO.LOW)
-
 	time.sleep(1)
-	print "Complete discharging of capacitance\n"
 	GPIO.setup(pin,GPIO.IN)
 	#count loops until voltage across capicator reads high on GPIO
 	while(GPIO.input(pin)==GPIO.LOW):
 		#print "My Pin current: ", GPIO.input(pin)
 		counter += 1
 		#time.sleep(0.1)
-	print GPIO.input(pin)
+	#print GPIO.input(pin)
 	return Resolve_Light(counter)
+
+def Motion_Sensing(pin):
+	GPIO.setup(pin,GPIO.IN)      # Echo
+
+	Current_State  = 0
+	Previous_State = 0
+	print "Waiting for PIR to settle ...", datetime.datetime.now()
+    # Loop until PIR output is 0
+    while GPIO.input(pin)==1:
+        Current_State  = 0
+    #wait 1 second before polling the pin
+    time.sleep(1)
+    return GPIO.input(pin)
+
 
 
 def main():
@@ -56,8 +70,8 @@ def main():
 	LIGHT_SENSING_PIN = 26
 	print "My measurement of strength of light via counting of loops :",RC_Analog(LIGHT_SENSING_PIN)
 
-
-
+	MOTION_SENSING_PIN = 13
+	print "Motion level : ",Motion_Sensing(MOTION_SENSING_PIN)
 
 
 if __name__ == '__main__':
