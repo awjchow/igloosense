97b6c7e210d111dbfc0c7ce2de18141b24cf1f57
awjchow <Anthony>
2015-02-01 17:42:39 -0500

    updating bluetooth

--- SETUP.sh
+++ SETUP.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get upgrade
 sudo apt-get install build-essential python-dev python-setuptools bluez pulseaudio-module-bluetooth python-bluez python-gobject python-gobject-2 bluez-tools
+sudo apt-get install libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev
 sudo easy_install -U distribute
 sudo apt-get install python-pip
 sudo pip install rpi.gpio
