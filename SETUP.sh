f77ab673a0cba91dac401ab8273ee8ab46b3ec10
awjchow <Anthony>
2015-02-01 16:10:19 -0500

    updating setup

--- SETUP.sh
+++ SETUP.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo apt-get upgrade
-sudo apt-get install build-essential python-dev python-setuptools
+sudo apt-get install build-essential python-dev python-setuptools bluez pulseaudio-module-bluetooth python-gobject python-gobject-2 bluez-tools
 sudo easy_install -U distribute
 sudo apt-get install python-pip
 sudo pip install rpi.gpio
-cd Temperature
-sudo python setup.py install
+#cd Temperature
+#sudo python setup.py install
