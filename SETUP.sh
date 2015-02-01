9f06be0b99831d5b29d7a34e097c58a1d2d5b645
awjchow <Anthony>
2015-02-01 17:51:40 -0500

    updating bluetooth

--- SETUP.sh
+++ SETUP.sh
@@ -6,5 +6,13 @@ sudo apt-get install libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical
 sudo easy_install -U distribute
 sudo apt-get install python-pip
 sudo pip install rpi.gpio
+
+wget https://www.kernel.org/pub/linux/bluetooth/bluez-5.27.tar.gz
+tar -xzvf bluez-5.27.tar.gz
+cd ../bluez-5.27
+./configure --prefix=/usr --sysconfdir=/etc  --localstatedir=/var --enable-experimental --enable-maintainer-mode --enable-library --disable-systemd
+make
+make check
+sudo make install
 #cd Temperature
 #sudo python setup.py install
