89daa38808020473e37666d23f7ae82b5de12ba4
awjchow <Anthony>
2015-02-02 19:13:57 -0500

    add bluetooth

--- test.py
+++ test.py
@@ -51,8 +51,11 @@ def MotionSensing(pin):
 def BluetoothDiscovery():
 	try:
 		nearby_devices = bluetooth.discover_devices(lookup_names = True)
+		for addr, name in nearby_devices:
+			print("  %s - %s" % (addr, name))
 		return nearby_devices
-	except:
+	except Exception,e:
+		print "Error with bluetooth discovery with error : ", e
 		return None
 
 
