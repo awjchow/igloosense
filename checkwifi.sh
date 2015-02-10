07d6746894a80b1f1fc7151aecda7a7e68f9ab9a
awjchow <Anthony>
2015-02-09 23:08:52 -0500

    Add wifi restart script

--- checkwifi.sh
+++ checkwifi.sh
@@ -5,7 +5,7 @@ ping -c4 google.com > /dev/null
 if [ $? != 0 ] 
 then
   echo "No network connection, restarting wlan0"
-  sudo /sbin/ifdown 'wlan0'
-  sleep 3
-  sudo /sbin/ifup --force 'wlan0'
+  sudo /sbin/ifdown wlan0
+  sleep 5
+  sudo /sbin/ifup --force wlan0
 fi
