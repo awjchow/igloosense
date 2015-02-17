9846f3ccbe0dcc85d2d1c04ebc18da447c7a5769
awjchow <Anthony>
2015-02-17 12:48:56 -0500

    do testing

--- testing.py
+++ testing.py
@@ -20,9 +20,8 @@ LOG_FILENAME = MY_LOG_DIRECTORY+'test.log'
 print "Creating log file at : " + LOG_FILENAME
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p',filename=LOG_FILENAME,level=logging.INFO)
 
-#uid = pwd.getpwnam("pi").pw_uid
-#gid = grp.getgrnam("pi").gr_gid
-#path = MY_LOG_DIRECTORY
-#os.chown(path, uid, gid)
+
+path = MY_LOG_DIRECTORY + 'test.log'
+os.chown(path, uid, gid)
 
 logging.warning("test log file")
