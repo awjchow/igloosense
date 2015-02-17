548d38d8316eeaf6e808980875a152f6d63cf015
awjchow <Anthony>
2015-02-17 12:46:54 -0500

    do testing

--- testing.py
+++ testing.py
@@ -6,9 +6,13 @@ import os
 
 MY_LOG_DIRECTORY = os.getcwd() + '/test-logs/'
 if not os.path.exists(MY_LOG_DIRECTORY):
-	os.makedirs(MY_LOG_DIRECTORY)
-	os.chmod(MY_LOG_DIRECTORY, 0777)
+	os.makedir(MY_LOG_DIRECTORY)
+	#os.chmod(MY_LOG_DIRECTORY, 0777)
 
+uid = pwd.getpwnam("pi").pw_uid
+gid = grp.getgrnam("pi").gr_gid
+path = MY_LOG_DIRECTORY
+os.chown(path, uid, gid)
 
 
 
@@ -16,9 +20,9 @@ LOG_FILENAME = MY_LOG_DIRECTORY+'test.log'
 print "Creating log file at : " + LOG_FILENAME
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p',filename=LOG_FILENAME,level=logging.INFO)
 
-uid = pwd.getpwnam("pi").pw_uid
-gid = grp.getgrnam("pi").gr_gid
-path = MY_LOG_DIRECTORY+'test.log'
-os.chown(path, uid, gid)
+#uid = pwd.getpwnam("pi").pw_uid
+#gid = grp.getgrnam("pi").gr_gid
+#path = MY_LOG_DIRECTORY
+#os.chown(path, uid, gid)
 
 logging.warning("test log file")
