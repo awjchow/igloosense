8268f6176cdc31c37984d620964e4aba40766008
awjchow <Anthony>
2015-02-17 12:38:55 -0500

    do testing

--- testing.py
+++ testing.py
@@ -1,3 +1,24 @@
+
+import logging
+import pwd
+import grp
 import os
 
-os.system(os.getcwd() + "/checkwifi.sh")
+MY_LOG_DIRECTORY = os.getcwd() + '/test-logs/'
+if not os.path.exists(MY_LOG_DIRECTORY):
+	os.makedirs(MY_LOG_DIRECTORY)
+	os.chmod(MY_LOG_DIRECTORY, 0777)
+
+
+
+
+LOG_FILENAME = MY_LOG_DIRECTORY+'test.log'
+print "Creating log file at : " + LOG_FILENAME
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p',filename=LOG_FILENAME,level=logging.INFO)
+
+uid = pwd.getpwnam("pi").pw_uid
+gid = grp.getgrnam("pi").gr_gid
+path = MY_LOG_DIRECTORY+'test.log'
+os.chown(path, uid, gid)
+
+logging.warning("test log file")
